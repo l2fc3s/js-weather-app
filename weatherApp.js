98a3daec5da847812b0c7a06fa144323728b1fb4
Larry LaMar <larrylamar@Larrys-iMac.lan>
2022-06-01 18:11:31 -0500

    completed overall functionality

--- weatherApp.js
+++ weatherApp.js
@@ -2,6 +2,11 @@ window.addEventListener("load", () => {
   let latitude;
   let longitude;
   const apiKey = "eee3276fd98cb728fa9c4303282c4b36";
+  let weatherCity = document.getElementById("weatherHeader");
+  let weatherInfo = document.getElementById("weatherCondition");
+  let currentTemp = document.getElementById("weatherTemp");
+  let tempHigh = document.getElementById("weatherHigh");
+  let lowTemp = document.getElementById("weatherLow");
 
   const success = (pos) => {
     latitude = pos.coords.latitude;
@@ -20,8 +25,19 @@ window.addEventListener("load", () => {
   }
 
   function fetchWeather() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
-    fetch(url).then((response) => response.json().then(console.log));
+    // fetch(url).then((response) => response.json().then(console.log));
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        weatherCity.innerHTML = `${data.name}`;
+        weatherInfo.innerHTML = `${data.weather[0].main}`;
+        currentTemp.innerHTML = `${data.main.temp}`;
+        tempHigh.innerHTML = `${data.main.temp_max} /`;
+        lowTemp.innerHTML = ` ${data.main.temp_min}`;
+      });
   }
 });
