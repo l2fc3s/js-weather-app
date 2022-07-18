4f0fd811b7d5da8deb14aa308163f42b3b61e36c
Larry LaMar <larrylamar@Larrys-iMac.lan>
2022-07-17 23:31:23 -0500

    worked on data cards and styling

--- weatherApp.js
+++ weatherApp.js
@@ -1,24 +1,3 @@
-let bgColorObject = {
-  "01d": "to bottom left, #d4fdff, #ffffff",
-  "01n": "to bottom left, #413074, #cccbcb",
-  "02d": "to bottom left, #ddecec, #ffffff",
-  "02n": "to bottom left, #726b8a, #949494",
-  "03d": "to bottom left, #cecece, #ffffff",
-  "03n": "to bottom left, #646075, #d3d3d3",
-  "04d": "to bottom left, #dde5e6, #ffffff",
-  "04n": "to bottom left, #63626d, #d3d3d3",
-  "09d": "to bottom left, #b7c9cc, #ffffff",
-  "09n": "to bottom left, #4d4a5c, #585858",
-  "10d": "to bottom left, #9da7a8, #ffffff",
-  "10n": "to bottom left, #474552, #3b3b3b",
-  "11d": "to bottom left, #777676, #afaeae",
-  "11n": "to bottom left, #363347, #2b2a2a",
-  "13d": "to bottom left, #ececec, #cecece",
-  "13n": "to bottom left, #5c5869, #d3d3d3",
-  "50d": "to bottom left, #ececec, #dddddd",
-  "50n": "to bottom left, #575764, #7c7c7c",
-};
-
 const changeBackgroundImage = (data) => {
   document.body.style.background = `url(./background-images/${data.weather[0].icon}.jpg)`;
   document.body.style.backgroundSize = "cover";
@@ -67,7 +46,9 @@ let forecast = document.getElementById("forecastList");
 let feelsLike = document.getElementById("feelsLike");
 let humidity = document.getElementById("humidity");
 let windSpeed = document.getElementById("windSpeed");
-let currentCondition = document.getElementById("currentCondition");
+let cloudPercentage = document.getElementById("cloudPercentage");
+let weatherVisibility = document.getElementById("visibility");
+let pressure = document.getElementById("pressure");
 
 window.addEventListener("load", () => {
   function showApiLoader(bool) {
@@ -109,22 +90,22 @@ window.addEventListener("load", () => {
         changeBackgroundImage(data);
 
         weatherCountry.innerHTML = `, ${data.sys.country}`;
-        weatherCity.innerHTML = `${data.name}`;
-        weatherInfo.innerHTML = `${data.weather[0].main}`;
+        weatherCity.innerHTML = data.name;
+        weatherInfo.innerHTML = data.weather[0].main;
         weatherIcon.innerHTML = `<img class='weather-icon' src="./weather-icons/${data.weather[0].icon}.png" alt="weather image"> `;
         currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;`;
         tempHigh.innerHTML = `H: ${Math.round(data.main.temp_max)}&deg;  `;
         lowTemp.innerHTML = ` L: ${Math.round(data.main.temp_min)}&deg;`;
-
         feelsLike.innerHTML = `${Math.round(data.main.feels_like)}&deg;`;
         humidity.innerHTML = `${data.main.humidity}%`;
-        windSpeed.innerHTML = `${Math.round(data.wind.speed)}`;
+        windSpeed.innerHTML = Math.round(data.wind.speed);
+        cloudPercentage.innerHTML = `${data.clouds.all}%`;
 
-        let conditionDescription = data.weather[0].description
-          .split(" ")
-          .map((word) => word[0].toUpperCase() + word.substring(1))
-          .join(" ");
-        currentCondition.innerHTML = conditionDescription;
+        let pressureConversion = data.main.pressure * 0.02952998597817832; // converts hpa to inHg
+        pressure.innerHTML = pressureConversion.toFixed(1);
+
+        let visibilityConversion = data.visibility / 1000 / 1.609;
+        weatherVisibility.innerHTML = Math.round(visibilityConversion);
       });
 
     // forecast api call
